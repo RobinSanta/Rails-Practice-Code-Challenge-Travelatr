984af4dc7749455f06d54b451a3b88bf862ce8c5
RobinSanta <robinsntn@gmail.com>
2019-11-27 15:17:52 -0800

    What we've done so far

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,23 @@
 class Blogger < ApplicationRecord
+	has_many :posts
+	has_many :destinations, through: :posts 
+	validates :name, uniqueness: true
+	validates :age, numericality: {greater_than: 0}
+	validates :bio, length: {minimum: 30}
+
+	def total_likes
+		all_posts = self.posts
+		post_map = all_posts.map do |post|
+			post.likes
+		end
+		post_map.sum
+	end
+
+	def featured_post
+		self.posts.max_by do |post|
+			post.likes
+		end
+	end
+
 
 end
